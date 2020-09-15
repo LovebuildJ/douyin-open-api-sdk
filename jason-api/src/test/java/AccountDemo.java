0c5b7ac2c297937ee30f79d1d50de0fc544cdf77
LovebuildJ <1141649370@qq.com>
2020-09-15 15:49:57 +0800

    账号授权

--- jason-api/src/test/java/AccountDemo.java
+++ jason-api/src/test/java/AccountDemo.java
@@ -1,19 +1,14 @@
-# douyin-open-api-sdk
-抖音Open Api 急速对接SDK， 简单，易用，高效！
+import com.douyin.open.models.Oauth2NewRefreshTokenInlineResponse200;
+import com.douyin.open.models.Oauth2RefreshTokenInlineResponse200;
+import com.douyin.open.models.ToutiaoOauth2Oauth2InlineResponse200;
+import com.douyin.open.models.ToutiaoOauth2Oauth2InlineResponse2002;
+import com.jason.open.api.account.AccountAuth;
 
-## SDK 优点
-- API覆盖率超过95%, 只需要简单的修改一下配置文件, 就能轻松调用api.
-- 自动集成官方SDK, 切换使用原生一样方便! 多种选择, 轻松适配!
-- 根据视频大小, 自动切换视频分片上传, 轻松避免异常, 保证接口安全.
-- 接口分类清晰, 操作简单明了
-
-
-
-## API手册
-```java
 /**
-* 账号授权
-*/
+ * @Description 账号授权
+ * @Date 2020/9/15 15:44
+ * @Author by 尘心
+ */
 public class AccountDemo {
 
     public static void main(String[] args) {
@@ -30,5 +25,3 @@ public class AccountDemo {
         Oauth2NewRefreshTokenInlineResponse200 refreshToken = auth.refreshRefreshToken("refreshToken");
     }
 }
-
-```
